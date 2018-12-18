17092dd36a1c5a163424933503354d65d8f511cf
hadar424 <hadar424@gmail.com>
2018-12-18 10:54:30 +0200

    project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "Expression.h"
+#include "Plus.h"
+#include "Number.h"
+#include "Mul.h"
+#include "Div.h"
+#include "ShuntingYard.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //Expression* e = new Plus(new Number(3), new Mul( new Div(new Number(4), new Number(2)) , new Number(5)));
+    //e->calculate();
+    string myExpression = " 2/(1-5)";
+    ShuntingYard* lalal = new ShuntingYard();
+    vector<char>* myPost= lalal->doPostfix(myExpression);
+    for(vector<char>::iterator it = myPost->begin(); it < myPost->end(); it++) {
+            cout << (*it);
+    }
     return 0;
 }
